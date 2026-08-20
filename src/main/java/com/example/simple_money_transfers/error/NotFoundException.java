76f5bb6c09cf6feeeb086f7b4c448ecdba8b2f6a
Marcus Vinícius Ibraim <mvibraim@gmail.com>
2026-08-20 13:15:21 -0300

    feat: RFC 9457 error contract with domain exception hierarchy

---
+++ src/main/java/com/example/simple_money_transfers/error/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.simple_money_transfers.error;
+
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+}
